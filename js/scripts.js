48ef03e365154a8f50b8f064961e7de33542c6f2
arnirohandi <164883142+arnirohandi@users.noreply.github.com>
2024-05-05 18:07:16 +0200

    Add 3 Pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-let pokemonList = [];
+let pokemonList = [
+    {
+        name: "Bulbasaur",
+        height: 0.7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: "Oddish",
+        height: 0.5,
+        types: ['grass', 'poison']
+    },
+    {
+        name: "Chansey",
+        height: 1.1,
+        types: ['normal']
+    }
+];
