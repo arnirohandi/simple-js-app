a3864eb1e782b80030df57d1e2bda92d1ab95e4a
arnirohandi <164883142+arnirohandi@users.noreply.github.com>
2024-05-10 22:08:02 +0200

    Clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -25,18 +25,11 @@ let pokemonList = [
 // Loop through the array and write each Pokémon name, height, and highlight special Pokémon
 for (let i = 0; i < pokemonList.length; i++) {
     let pokemon = pokemonList[i];
-    // let pokemonInfo = pokemon.name + " (height: " + pokemon.height + ")";
 
     // Check if the height is above a certain value to highlight special Pokémon
     // if (pokemon.height > 1.5) {
     //     pokemonInfo += " - Wow, that's big!";
     // }
-
-    // <div class="pokemon">
-    //     <p class="name">Bulbasaur</p>
-    //     <p class="height">(Height: 0.7)</p>
-    // </div>
-
 
     document.write("<div class='pokemon'>" + 
     "<p class='name'>" + pokemon.name + "</p>" + 
@@ -44,5 +37,3 @@ for (let i = 0; i < pokemonList.length; i++) {
     "</div>");
     
 }
-
-// document.write("pisang <b>pikachu</b>")
