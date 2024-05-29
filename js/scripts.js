7b6b8d1a425788e6a8508105bf870b42cff492ab
arnirohandi <164883142+arnirohandi@users.noreply.github.com>
2024-05-29 23:21:06 +0200

    Add showModal function

--- js/scripts.js
+++ js/scripts.js
@@ -105,7 +105,7 @@ let pokemonRepository = (function () {
   */
   function showDetails(pokemon) {
     loadDetails(pokemon);
-    console.log(pokemon);
+    showModal(pokemon.name, 'Height: ' + pokemon.height);
   }
 
   return {
@@ -123,3 +123,41 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+function showModal(title, text) {
+  let modalContainer = document.querySelector('#modal-container');
+
+  // Clear all existing modal content
+  modalContainer.innerHTML = '';
+
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  // Add the new modal content
+  let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+  closeButtonElement.addEventListener('click', hideModal);
+  
+  let titleElement = document.createElement('h1');
+  titleElement.innerText = title;
+
+  let contentElement = document.createElement('p');
+  contentElement.innerText = text;
+
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(contentElement);
+  modalContainer.appendChild(modal);
+
+  modalContainer.classList.add('is-visible');
+}
+
+document.querySelector('#show-modal').addEventListener('click', () => {
+  showModal('Judul', 'Isi dari modal');
+});
+
+function hideModal() {
+  let modalContainer = document.querySelector('#modal-container');
+  modalContainer.classList.remove('is-visible');
+}
